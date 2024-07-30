54df07b682dd37c2702ab7a9386995d5c95f18f4
alexkcode <alexiskwan.uiuc@gmail.com>
2024-07-30 16:06:39 -0500

    updating unit test

--- airflow/tests/test_scraper.py
+++ airflow/tests/test_scraper.py
@@ -1,7 +1,8 @@
 import pytest
 import dags.scraper
 
-def test_scrape():
+def test_scraper_has_valid_output():
     return_value = dags.scraper.scrape_yelp_reviews()
     print(return_value)
     assert return_value is not None
+    assert False
